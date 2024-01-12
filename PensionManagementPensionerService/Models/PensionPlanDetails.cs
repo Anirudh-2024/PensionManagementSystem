3e2d62828267dcad33e87de4d873d9aa487957e6
Sivaadda <sivapriyaadda@gmail.com>
2024-01-12 15:34:27 +0530

    added properties to model classes.

--- PensionManagementPensionerService/Models/PensionPlanDetails.cs
+++ PensionManagementPensionerService/Models/PensionPlanDetails.cs
@@ -1,6 +1,15 @@
-﻿namespace PensionManagementPensionerService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PensionManagementPensionerService.Models
 {
     public class PensionPlanDetails
     {
+        [Key]
+        public Guid PensionPlanId { get; set; }
+        public string PensionPlanName { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string PensionDetails { get; set; }
     }
 }
