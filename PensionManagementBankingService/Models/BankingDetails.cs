33b3e90897bbb0644d596c09b9ff0f63fd909757
Ajay <nagaajaykumar.polisetti@cognizant.com>
2024-01-12 15:21:51 +0530

    Added Properties to model classes.

--- PensionManagementBankingService/Models/BankingDetails.cs
+++ PensionManagementBankingService/Models/BankingDetails.cs
@@ -1,6 +1,25 @@
-﻿namespace PensionManagementBankingService.Models
+﻿using PensionManagementPensionerService.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PensionManagementBankingService.Models
 {
     public class BankingDetails
     {
+        [Key]
+        public Guid BankId { get; set; }
+        public string BankName { get; set; }
+        public string AccountNumber { get; set;}
+        public string IfscCode { get; set;}
+        public string BranchName{ get; set;}
+        public string PanNumber { get; set;}
+
+    
+        public string PensionerId { get; set; }
+
+        [ForeignKey("PensionerId")]
+        public virtual PensionerDetails PensionerDetails { get; set; }
+
+
     }
 }
