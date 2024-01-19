dbb8ec090105cb50029affceff4d058bd2428b4c
Sivaadda <sivapriyaadda@gmail.com>
2024-01-19 17:24:20 +0530

    Added DbContext, created migration and updated some changes in property datatypes

--- PensionManagementPensionerService/Models/GuardianDetails.cs
+++ PensionManagementPensionerService/Models/GuardianDetails.cs
@@ -8,12 +8,12 @@ namespace PensionManagementPensionerService.Models
         [Key]
         public Guid GuardianId { get; set; }
         public string GuardianName { get; set; }
-        public DateOnly DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string Relation { get; set; }
         public int Age { get; set; }
         public string Gender { get; set; }
         public string PhoneNumber { get; set; }
-        public string PensionerId { get; set; }
+        public Guid PensionerId { get; set; }
 
         [ForeignKey("PensionerId")]
         public virtual PensionerDetails PensionerDetails { get; set; }
--- PensionManagementPensionerService/Models/PensionerDetails.cs
+++ PensionManagementPensionerService/Models/PensionerDetails.cs
@@ -9,14 +9,14 @@ namespace PensionManagementPensionerService.Models
         [Key]
         public Guid PensionerId { get; set; }
         public string FullName { get; set; }
-        public DateOnly DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string Gender { get; set; }
         public string AadharNumber { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public int Age { get; set; }
         public int UserId { get; set; }
-        public string PensionPlanId { get; set; }
+        public Guid PensionPlanId { get; set; }
 
         [ForeignKey("UserId")]
         public virtual UserDetails UserDetails { get; set; }
