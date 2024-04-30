d183257619b69f29e735ec23facabb59180a143c
Yashwanth <ananthayashwanth143@gmail.com>
2024-04-30 13:46:30 +0530

    Added AutoMapper to Banking

---
+++ PensionManagementBankingService/AutoMapper/BankingMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PensionManagementBankingService.DTO;
+using PensionManagementBankingService.Models;
+
+
+namespace PensionManagementBankingService.AutoMapper
+{
+    public class BankingMapping:Profile
+    {
+        public BankingMapping()
+        {
+            CreateMap<BankRequest, BankingDetails>().ReverseMap();
+            CreateMap<BankResponse, BankingDetails>().ReverseMap();
+        }
+        
+
+
+
+
+    }
+}
